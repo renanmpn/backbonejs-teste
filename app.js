0b1cbc4cab2288a056e4e2bb7b28f1612ca1aa38
renanmpn <renanmpn@hotmail.com>
2016-02-22 10:42:22 -0300

    - Adding the data to the project - Created the backend with the get and post endpoint

---
+++ app.js
@@ -0,0 +1,90 @@
+var fs = require('fs');
+
+var express = require("express");
+var app     = express();
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
+app.use(express.static(__dirname + '/public'));
+
+var categories = JSON.parse(fs.readFileSync('./data/categories.json', 'utf8'));
+var boards = JSON.parse(fs.readFileSync('./data/boards.json', 'utf8'));
+
+categories.sort(_compareName);
+
+function _compareName(a,b) {
+  if (a.name < b.name)
+    return -1;
+  else if (a.name > b.name)
+    return 1;
+  else 
+    return 0;
+}
+
+function _compareDefault(a,b){
+    if(a.default == true && b.default == false){
+        return -1;
+    }else if (a.default == false && b.default == true){
+        return 1;
+    }else {
+        return 0;
+    }
+}
+
+function _filterByCategories(categorie){
+    return function(obj){
+        if('category_id' in obj && obj.category_id == categorie){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+    
+/**
+ * Check if the category exists
+ */
+function _checkCategory(cat){
+    for(var x of categories){
+        if(x.id == cat){
+            return true;
+        }
+    }
+    return false;
+}    
+    
+/**
+ * GET function to get all the categories
+ */
+app.get('/categories',function(req,res){
+    res.status(200).jsonp(categories);
+});
+
+
+/**
+ * POST function that return all the boards based on the category passed
+ */
+app.post('/boards',function(req,res){
+    var category = req.body.category;
+    if(category){
+        if(!isNaN(category)){            
+            if(_checkCategory(category)){
+                res.status(200).jsonp(boards.filter(_filterByCategories(category)).sort(_compareDefault));    
+            }else{
+                res.status(500).jsonp(error:true,message:'This category does not exist.');    
+            }
+        }else{
+            res.status(500).jsonp(error:true,message:'Category field is not a number.');    
+        }
+    }else{
+        res.status(500).jsonp(error:true,message:'Category field is missing. Please check your POST call.');
+    }
+});
+
+app.listen(3000);
+console.log("Running at Port 3000");
